4b0d54c7fc6e562c59454adeb37dd94c1847446b
ngandinhtk <ngandinhtk@gmail.com>
2026-01-04 05:32:17 +0000

    commit new

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Trash2, Pin } from 'lucide-react';
 
